025b119f6d5785c4d86041f5601a9045b73cc6a9
Jeremias Montoya <83260187+jeremontoya155@users.noreply.github.com>
2022-03-28 21:54:05 -0300

    Ejemplo Fibonacci

---
+++ src/main/java/test/fibonacci/testUno.java
@@ -0,0 +1,88 @@
+
+package test.fibonacci;
+
+import java.util.Scanner;
+
+
+public class testUno {
+    
+    
+    public static void main(String[] args) {
+        
+        //La serie de fibonacci consiste en una sucesion numerica conformada
+        //por la suma de sus 2 anteriores
+        //Ejemplo
+        //n=7
+        //0 1 1 2 3 5 8
+        
+        
+        //int rango=cargarNumero("Ingrese el rango de fibonacci");
+        
+        int rango=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que desee:"));
+        
+        Fibonacci(rango);
+        
+    
+        
+        
+    }
+
+
+public static int  cargarNumero(String texto){
+
+    Scanner sn= new Scanner(System.in);
+    
+    System.out.println(texto);
+    
+    
+    int numero=sn.nextInt();
+    
+    return numero;
+
+}
+
+
+private static void Fibonacci(int rango) {
+
+   
+    int aux1=0;
+    int aux2=1;
+    int res;
+    
+    
+    /*do {        
+        
+        System.out.println(aux1);
+        
+        res=aux1+aux2;
+        aux1=aux2;
+        aux2=res;
+        contador+=1;
+        
+        
+        System.out.println(res);
+        
+    } while (contador<rango);*/
+    
+    for (int i = 0; i < rango; i++) {
+
+            System.out.println(aux1);
+            
+            res=aux1+aux2;
+            aux1=aux2;
+            aux2=res;
+            
+            
+           
+            
+    }
+    
+
+}
+    
+
+
+    
+}
+
+
